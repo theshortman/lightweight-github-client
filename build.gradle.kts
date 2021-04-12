7e0c516a62263bbc464a1a6b2bfd9862323d4e54
theshortman <theshortman@protonmail.com>
2021-04-12 16:03:55 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("js") version "1.4.32"
+}
+
+group = "me.iam"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlin/p/kotlin/kotlin-js-wrappers") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-js"))
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
+}
+
+kotlin {
+    js(IR) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "lightweight-github-client"
+

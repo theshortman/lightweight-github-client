63370c23f337d99df2f30c9fb8194244cee90b8d
theshortman <theshortman@protonmail.com>
2021-04-16 15:51:08 +0300

    build: add ktor and styled dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("js") version "1.4.32"
+    kotlin("plugin.serialization") version "1.4.31"
 }
 
 group = "me.iam"
@@ -15,6 +16,16 @@ dependencies {
     testImplementation(kotlin("test-js"))
     implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
     implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
+
+    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
+    implementation(npm("styled-components", "~5.2.1"))
+
+    val ktorVersion= "1.5.3"
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-client-js:$ktorVersion")
+    implementation(kotlin("stdlib-js"))
+
 }
 
 kotlin {

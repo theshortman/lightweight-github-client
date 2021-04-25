95106b14b1187aa5c3e915043ca77916adf775a2
theshortman <theshortman@protonmail.com>
2021-04-25 15:50:11 +0300

    fix: correct name of repository

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 const val GITHUB_ACCESS_TOKEN = "your_github_access_token"
 
 val trackedRepositories = listOf(
-    " theshortman/lightweight-github-client",
+    "theshortman/lightweight-github-client",
 )
